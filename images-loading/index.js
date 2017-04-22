943e2f9c9d68d107dd707b83d651aec5bca34165
Stanislav Sysoev <d4rkr00t@gmail.com>
2017-04-23 09:19:44 +1000

    Add image lodaing example

---
+++ images-loading/index.js
@@ -0,0 +1,10 @@
+import './style.css';
+import bike from './ic_directions_bike_48px_js.svg';
+import car from './ic_directions_car_48px_js.svg';
+import railway from './ic_directions_railway_48px_js.svg';
+import boat from './ic_directions_boat_48px_js.svg';
+
+document.querySelector('.icon-js-bike').style.background = `url(${bike})`;
+document.querySelector('.icon-js-car').style.background = `url(${car})`;
+document.querySelector('.icon-js-railway').style.background = `url(${railway})`;
+document.querySelector('.icon-js-boat').style.background = `url(${boat})`;
